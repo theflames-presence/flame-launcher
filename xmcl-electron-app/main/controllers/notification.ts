f75f9db1a09ab6dcf283106b7977e7b60b8f76a6
aislxflames <proarghaop@gmail.com>
2024-02-28 15:32:31 +0530

    v1.0.4

--- xmcl-electron-app/main/controllers/notification.ts
+++ xmcl-electron-app/main/controllers/notification.ts
@@ -5,7 +5,7 @@ import { ControllerPlugin } from './plugin'
 export const notificationSetupPlugin: ControllerPlugin = function (this: ElectronController) {
   this.app.once('engine-ready', () => {
     if (this.app.platform.os === 'windows') {
-      app.setAppUserModelId('X Minecraft Launcher')
+      app.setAppUserModelId('Flame Launcher')
     }
   })
 }
